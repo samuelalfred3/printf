0f1f9645d142d872593515bb4447cc8e47c8ba54
Ochiengthegreat <ochiengroy49@gmail.com>
2023-09-21 05:55:59 +0000

    Update

--- _printf.c
+++ _printf.c
@@ -13,8 +13,6 @@ int _printf(const char *format, ...)
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
 	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
